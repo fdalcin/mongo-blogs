0c4abb9697f9ddc6ff76f61edd7f8e78044ea6c6
yanalexander <yan.venera@univali.br>
2019-02-23 17:56:17 -0300

    Add blog structure;

---
+++ src/main/java/br/com/mongoblogs/model/Blog.java
@@ -0,0 +1,28 @@
+package br.com.mongoblogs.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "blogs")
+public class Blog {
+
+    @Id
+    private String id;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/br/com/mongoblogs/web/controller/BlogController.java
@@ -0,0 +1,10 @@
+package br.com.mongoblogs.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blogs")
+public class BlogController {
+
+}
