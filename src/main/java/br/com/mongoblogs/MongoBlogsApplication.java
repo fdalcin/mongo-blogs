e7275bbf778d5a9705361f3bf03d559206f03e67
Felipe Dalcin <felipe.dalcin@outlook.com>
2019-02-23 13:59:34 -0300

    Adding maven configurations

---
+++ src/main/java/br/com/mongoblogs/MongoBlogsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mongoblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBlogsApplication.class, args);
+	}
+
+}
