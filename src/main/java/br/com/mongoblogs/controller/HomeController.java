17589e8496148edaa4fbd09425c2801a828e83e0
Felipe Dalcin <felipe.dalcin@outlook.com>
2019-03-09 11:34:30 -0300

    Listing all blogs on first page

--- src/main/java/br/com/mongoblogs/controller/HomeController.java
+++ src/main/java/br/com/mongoblogs/controller/HomeController.java
@@ -1,17 +1,29 @@
 package br.com.mongoblogs.controller;
 
+import br.com.mongoblogs.model.Blog;
+import br.com.mongoblogs.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @Scope("session")
 public class HomeController
 {
+    @Autowired
+    private BlogRepository blogRepository;
+
     @GetMapping("/")
-    public String index(Model view)
+    public String index(ModelMap model)
     {
+        List<Blog> blogs = this.blogRepository.findAll();
+
+        model.addAttribute("blogs", blogs);
+
         return "index";
     }
 
